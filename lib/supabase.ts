bc69109f5fb9043dba426f233b45158e7bea1a3b
Chuah Hui Wen <chwen0912@gmail.com>
2025-11-02 11:41:10 +0800

    Update supabase.ts

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,14 +1,24 @@
 import { createClient } from '@supabase/supabase-js'
-import AsyncStorage from '@react-native-async-storage/async-storage'
+import { Platform } from 'react-native'
+
+let AsyncStorage: any
+if (Platform.OS !== 'web') {
+  AsyncStorage = require('@react-native-async-storage/async-storage').default
+}
 
 const supabaseUrl = 'https://tqkccugnneulsopycjap.supabase.co'
 const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRxa2NjdWdubmV1bHNvcHljamFwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjE3MjkyNDQsImV4cCI6MjA3NzMwNTI0NH0.FkIzRcZlPyBNLXxXmC9Xacg0xXg2JdJCbYcZsJND__E'
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
-  auth: {
-    storage: AsyncStorage,
-    autoRefreshToken: true,
-    persistSession: true,
-    detectSessionInUrl: false,
-  },
-})
+const options =
+  Platform.OS === 'web'
+    ? {} // web automatically uses localStorage
+    : {
+        auth: {
+          storage: AsyncStorage,
+          autoRefreshToken: true,
+          persistSession: true,
+          detectSessionInUrl: false,
+        },
+      }
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey, options)
